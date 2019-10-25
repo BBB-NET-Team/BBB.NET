9bb899952dbeb5b1fdc079696eeaa5f327600066
Royce551 <poohwie2005@gmail.com>
2019-10-24 21:58:19 -0500

    Populate MainAsync() with things (Should be enough to make the bot log in)

--- BBB.NET/BBB.NET/Program.cs
+++ BBB.NET/BBB.NET/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
 namespace BBB.NET
 {
     public class Program
@@ -8,6 +12,23 @@ namespace BBB.NET
         static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
         public async Task MainAsync()
         {
+            
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+
+                client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                
+                await client.LoginAsync(TokenType.Bot, "token"); // Token goes here, don't hardcode this (obviously)
+                await client.StartAsync();
+
+                // Here we initialize the logic required to register our commands.
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(-1);
+            }
         }
         private Task Log(LogMessage msg)
         {
