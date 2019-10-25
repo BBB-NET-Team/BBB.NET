0e3f3bf02a147ee714ee060c62d746ce61274bd5
Royce551 <poohwie2005@gmail.com>
2019-10-24 21:51:49 -0500

    Create logging method (as per documentation, logs to command line output)

--- BBB.NET/BBB.NET/Program.cs
+++ BBB.NET/BBB.NET/Program.cs
@@ -3,9 +3,16 @@ using Discord;
 using Discord.WebSocket;
 namespace BBB.NET
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
-
+        public async Task MainAsync()
+        {
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
     }
 }
