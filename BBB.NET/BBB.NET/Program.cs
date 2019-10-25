5eca8b591149fb116ae652d2a79a668e775aaa18
Royce551 <poohwie2005@gmail.com>
2019-10-24 22:00:41 -0500

    MainAsync() commit Ver. 2

--- BBB.NET/BBB.NET/Program.cs
+++ BBB.NET/BBB.NET/Program.cs
@@ -10,25 +10,26 @@ namespace BBB.NET
     public class Program
     {
         static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+
         public async Task MainAsync()
         {
-            
-            using (var services = ConfigureServices())
-            {
-                var client = services.GetRequiredService<DiscordSocketClient>();
+            _client = new DiscordSocketClient();
 
-                client.Log += LogAsync;
-                services.GetRequiredService<CommandService>().Log += LogAsync;
+            _client.Log += Log;
 
-                
-                await client.LoginAsync(TokenType.Bot, "token"); // Token goes here, don't hardcode this (obviously)
-                await client.StartAsync();
+            // Remember to keep token private or to read it from an 
+            // external source! In this case, we are reading the token 
+            // from an environment variable. If you do not know how to set-up
+            // environment variables, you may find more information on the 
+            // Internet or by using other methods such as reading from 
+            // a configuration.
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
 
-                // Here we initialize the logic required to register our commands.
-                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
-
-                await Task.Delay(-1);
-            }
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
         private Task Log(LogMessage msg)
         {
