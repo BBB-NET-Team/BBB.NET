583cbe1fd3e0bb5ae2db44ce5f385bb29b0dce1c
Royce551 <poohwie2005@gmail.com>
2019-10-26 16:27:58 -0500

    Fix the todo comment

--- BBB.NET/BBB.NET/Program.cs
+++ BBB.NET/BBB.NET/Program.cs
@@ -7,7 +7,7 @@ using Discord.WebSocket;
 using Discord.Commands;
 namespace BBB.NET
 {
-    public class Program
+    public class Program // uwu i love you goshunjin-sama~ <3
     {
         static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
         private DiscordSocketClient _client;
@@ -19,7 +19,7 @@ namespace BBB.NET
             _client.Log += Log;
 
             await _client.LoginAsync(TokenType.Bot,
-                "token"); //Change this later to get this from a configuration file
+                "token"); // TODO: get from a configuration file. As we all know, hardcoding shit is a BAAAAAAAADDDDDDDD idea.
             await _client.StartAsync();
 
             // Block this task until the program is closed.
