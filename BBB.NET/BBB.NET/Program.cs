041b418f5d510173e116467d1b4641a9f0a74928
Royce551 <poohwie2005@gmail.com>
2019-10-27 14:12:54 -0500

    Added a way to parse a config file; get token from config file

--- BBB.NET/BBB.NET/Program.cs
+++ BBB.NET/BBB.NET/Program.cs
@@ -12,15 +12,25 @@ namespace BBB.NET
     {
         static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
         private DiscordSocketClient _client;
-
+        public (string, string, string, string, string) ParseConfig()
+        {
+            var json = System.IO.File.ReadAllText("config.json");
+            dynamic jToken = Newtonsoft.Json.Linq.JToken.Parse(json);
+            string prefix = jToken.prefix;
+            string version = jToken.version;
+            string color = jToken.color;
+            string directory = jToken.directory;
+            string token = jToken.token;
+            return (prefix, version, color, directory, token);
+        }
         public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
 
             _client.Log += Log;
-
+            var token = ParseConfig();
             await _client.LoginAsync(TokenType.Bot,
-                "token"); // TODO: get from a configuration file. As we all know, hardcoding shit is a BAAAAAAAADDDDDDDD idea.
+                token.Item5); // TODO: get from a configuration file. As we all know, hardcoding shit is a BAAAAAAAADDDDDDDD idea.
             await _client.StartAsync();
 
             // Block this task until the program is closed.
